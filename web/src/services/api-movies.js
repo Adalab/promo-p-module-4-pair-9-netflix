1ae51d22abc080edb269fdbdb482f67c079b0367
“CarlotaBlanco” <lotsilots@gmail.com>
2022-05-18 11:19:01 +0200

    add exercises day 5

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -2,11 +2,14 @@
 
 const getMoviesFromApi = (value) => {
   console.log(value);
-  console.log("Se están pidiendo las películas de la app");
+  console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(`//localhost:4000/movies?gender=${value.gender}`, {
-    method: "GET",
-  })
+  return fetch(
+    `//localhost:4000/movies?gender=${value.gender}&sort=${value.sort}`,
+    {
+      method: 'GET',
+    }
+  )
     .then((response) => response.json())
     .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
